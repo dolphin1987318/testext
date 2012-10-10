00e2a9f425085a0eac91a626246a48e247714f44
wxf <allen_wangxufeng@163.com>
2012-10-10 17:38:44 +0800

    extjs test

---
+++ app.js
@@ -0,0 +1,25 @@
+Ext.application({
+    name: 'HelloExt',
+    launch: function() {
+	Ext.create('Ext.panel.Panel', {
+	    renderTo: Ext.getBody(),
+	    width: 400,
+	    height: 300,
+	    title: 'Container Panel',
+	    items: [
+		{
+		    xtype: 'panel',
+		    title: 'Child Panel 1',
+		    height: 100,
+		    width: '75%'
+		},
+		{
+		    xtype: 'panel',
+		    title: 'Child Panel 2',
+		    height: 100,
+		    width: '75%'
+		}
+	    ]
+	});
+    }
+});
